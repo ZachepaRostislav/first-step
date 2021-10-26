cb833912976d5c6401e35083a6c197a947ef9f0c
rzachepa <telp.r.zachepa@gmail.com>
2021-10-26 15:40:56 +0300

    DC7CI7R5IBT

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,14 +7,16 @@ export default class Header extends Component {
 
   render() {
     return (
-      <header className={style.header}>
-        <nav className={style.navigation}>
-          <a className={style.link} href="#">First Page</a>
-          <a className={style.link} href="#">Second Page</a>
-          <a className={style.link} href="#">Third Page</a>
-          <a className={style.link} href="#">Fourth Page</a>
-        </nav>
-      </header>      
+      <>
+        <header className={style.header}>
+          <nav className={style.navigation}>
+            <a className={style.link} href="#">First Page</a>
+            <a className={style.link} href="#">Second Page</a>
+            <a className={style.link} href="#">Third Page</a>
+            <a className={style.link} href="#">Fourth Page</a>
+          </nav>
+        </header>
+      </>
     )
   }
 }
